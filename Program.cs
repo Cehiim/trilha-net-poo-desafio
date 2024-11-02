ff88b5dd5a79dc8a7b886a874660a3919cc46911
Cesar Imai <cesarhi.imai@gmail.com>
2024-11-02 11:27:23 -0300

    tarefa finalizada

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\nTestando Nokia:");
+Nokia meuNokia = new Nokia("123-456", "Nokia 3310", "001122334455", 64);
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\nTestando Iphone:");
+Iphone meuIphone = new Iphone("789-101", "iPhone 13", "998877665544", 128);
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("Instagram");
